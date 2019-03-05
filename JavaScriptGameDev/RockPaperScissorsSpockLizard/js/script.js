f1589e972df5d5f2cfafa064f2cc6a58232e29e5
Vladislav <venik_lapochkin@mail.ru>
2019-03-05 16:28:54 +0300

    some fixes and refactoring

--- JavaScriptGameDev/RockPaperScissorsSpockLizard/js/script.js
+++ JavaScriptGameDev/RockPaperScissorsSpockLizard/js/script.js
@@ -52,15 +52,16 @@ jQuery('document').ready(() => {
 			++userVictories;
 			if (userVictories == 1) {
 				document.getElementById("matchUser1").classList.add("winMatch");
+				document.querySelector(".result > p").classList.add("winMatch");
 			}
 			else if (userVictories == 2) {
 				document.getElementById("matchUser2").classList.add("winMatch");
+				document.querySelector(".result > p").classList.add("winMatch");
 			}
 			else if (userVictories == 3) {
 				document.getElementById("matchUser3").classList.add("winMatch");
 			}
 			result.innerHTML = "USER WIN MATCH";
-			document.querySelector(".result > p").classList.add("winMatch");
 			setTimeout(() => resetScoreBoard(), 1000);
 			setTimeout(() => sleep(1000), 1);
 			console.log("userVictories: " + userVictories);
@@ -69,15 +70,16 @@ jQuery('document').ready(() => {
 			++computerVictories;
 			if (computerVictories == 1) {
 				document.getElementById("matchComputer1").classList.add("winMatch");
+				document.querySelector(".result > p").classList.add("loseMatch");
 			}
 			else if (computerVictories == 2) {
 				document.getElementById("matchComputer2").classList.add("winMatch");
+				document.querySelector(".result > p").classList.add("loseMatch");
 			}
 			else if (computerVictories == 3) {
 				document.getElementById("matchComputer3").classList.add("winMatch");
 			}
 			result.innerHTML = "COMPUTER WIN MATCH";
-			document.querySelector(".result > p").classList.add("loseMatch");
 			setTimeout(() => resetScoreBoard(), 1000);
 			setTimeout(() => sleep(1000), 1);
 			console.log("computerVictories: " + computerVictories);
@@ -86,26 +88,16 @@ jQuery('document').ready(() => {
 
 	function createEndGame() {
 		if (userVictories == 3 || computerVictories == 3) {
-			document.querySelector(".result > p").classList.remove("winMatch","loseMatch","winGame");
 			document.querySelector(".result > p").classList.add("winGame");
 			if (userVictories == 3 && computerVictories != 3) {
 				result.innerHTML = "USER WIN GAME";
-				setTimeout(() => resetScoreBoard(), 1000);
-				setTimeout(() => sleep(1000), 1);
-				userVictories = 0;
-				computerVictories = 0;
-				document.getElementById("matches").classList.add("matchesClear");
 				console.log("USER WIN GAME");
 			}
 			else if (userVictories != 3 && computerVictories == 3) {
 				result.innerHTML = "COMPUTER WIN GAME";
-				setTimeout(() => resetScoreBoard(), 1000);
-				setTimeout(() => sleep(1000), 1);
-				userVictories = 0;
-				computerVictories = 0;
-				document.getElementById("matches").classList.add("matchesClear");
 				console.log("COMPUTER WIN GAME");
 			}
+			setTimeout(() => newGame(), 1000);
 		}
 	}
 
@@ -118,9 +110,19 @@ jQuery('document').ready(() => {
 		document.querySelector(".result > p").classList.remove("winMatch","loseMatch","winGame");
 	}
 
+	function newGame() {
+		var elements = document.getElementsByClassName('winMatch');
+		while (elements.length > 0) {
+   			elements[0].classList.remove('winMatch');
+		}
+		resetScoreBoard();
+		setTimeout(() => sleep(1000), 1);
+		userVictories = 0;
+		computerVictories = 0;
+	}
+
 	function createGameStrategy(userChoice) {
 		const computerChoice = getComputerChoice();
-
 		switch (userChoice + computerChoice) {
 			case "rockscissors":
 			case "rocklizard":
@@ -162,12 +164,10 @@ jQuery('document').ready(() => {
 			console.log("DRAW");
 			showDraw(userChoice);
 			break;
-
 		}
 		createGameMatch();
 		createEndGame();
 	}
-
 
 	function createIconsListener() {
 		rock.addEventListener("click", () => createGameStrategy("rock"));	
